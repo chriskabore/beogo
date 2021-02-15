ebd1672f276f3e55aad4a5d8ba13575a7360b47a
Christian Kabore <chriskabor@gmail.com>
2021-02-15 19:34:44 +0000

    added config file i18n.js for i18n support

--- athenea-app/src/App.js
+++ athenea-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Greeting = props => <p>Hello {props.name}!</p>
+
+const App = props => (
+    <>
+      <div>
+        <h1>Athenea M&E</h1>
+        <br/>
+        <Greeting name="Christian" />
+      </div>
+    </>
+);
 
 export default App;
