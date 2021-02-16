69404e2a4994cff76a4d46cc02f72a6692bcfb43
Christian Kabore <chriskabor@gmail.com>
2021-02-16 10:54:13 +0000

    added AtheneaRoute.js

--- athenea-app/src/utils/routing/AtheneaRoute.js
+++ athenea-app/src/utils/routing/AtheneaRoute.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
 
 const AtheneaRoute = ({ component: Component, restricted, ...rest }) => (
+
     // restricted = false meaning public route
     // restricted = true meaning restricted route
-    <Route {...rest} render={props => (isLogin() && restricted ?
+    <Route {...rest} render={props => ( restricted ?
         <Redirect to="/home" /> : <Component {...props} />)} />
-)
+);
 
 export default AtheneaRoute;
