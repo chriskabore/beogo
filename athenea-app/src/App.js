e3142bc7cc6ae244e2ff55e96550079434209a6a
Christian Kabore <chriskabor@gmail.com>
2021-02-16 07:58:41 +0000

    added project folder structure (scafolding)

--- athenea-app/src/App.js
+++ athenea-app/src/App.js
@@ -1,15 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
-const Greeting = props => <p>Hello {props.name}!</p>
-
-const App = props => (
-    <>
-      <div>
-        <h1>Athenea M&E</h1>
-        <br/>
-        <Greeting name="Christian" />
-      </div>
-    </>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
