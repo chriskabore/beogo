6453b808ad469db197c56908378ed8d1ad93e92a
Christian Kabore <chriskabor@gmail.com>
2021-02-16 08:02:10 +0000

    added layout components: HeaderComponent.jsx, FooterComponent.jsx, SideBarComponent.jsx, ContentComponent.jsx

---
+++ athenea-app/src/components/layout/ContentComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ContentComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3>content</h3>
+            </div>
+        );
+    }
+}
+
+export default ContentComponent;
