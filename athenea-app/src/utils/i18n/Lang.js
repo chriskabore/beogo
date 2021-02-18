eccadce5e16aee4f5c307a7d2e8dfc7d3d890cb3
Christian Kabore <chriskabor@gmail.com>
2021-02-18 14:22:14 +0000

    removed module athenea-app

--- athenea-app/src/utils/i18n/Lang.js
+++
@@ -1,7 +0,0 @@
-
-const languageOptions = [
-    { key: "English", text: "English", value: "EN" },
-    { key: "French", text: "Français", value: "FR" },
-    { key: "Spanish", text: "Español", value: "ES" }
-];
-
--- athenea-app/src/utils/routing/AuthenticatedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Route, Redirect, Switch} from 'react-router-dom';
-import Auth from '../authentication/Auth';
-
-const AuthenticatedRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (Auth.getAuth() ? <Component {...props} /> : <Redirect to="/sign-in" />)} />
-);
-
-export default AuthenticatedRoute;
